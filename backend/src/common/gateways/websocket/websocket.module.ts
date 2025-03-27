e32f74287cf87c98492c3afe9bc63fac71052ffa
mrudulks <devmrudulks@gmail.com>
2025-03-27 14:26:33 +0530

    new commit

--- backend/src/common/gateways/websocket/websocket.module.ts
+++ backend/src/common/gateways/websocket/websocket.module.ts
@@ -1,4 +1,9 @@
 import { Module } from '@nestjs/common';
+import { CommonWebSocketGateway } from './websocket.gateway';
+import { JwtService } from '@nestjs/jwt';
 
-@Module({})
+@Module({
+  providers: [CommonWebSocketGateway, JwtService],
+  exports: [CommonWebSocketGateway],
+})
 export class WebsocketModule {}
